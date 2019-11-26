a408dac9256271ad94d7c5532fc78c9827249f6a
Iván <Ivn@10.100.41.132>
2019-11-26 18:07:30 +0100

    changes

--- mainClass.py
+++ mainClass.py
@@ -5,6 +5,5 @@ print(cv2.__version__)
 img = cv2.imread("img/mango-de-frente.png", 1)
 img2 = cv2.imread("img/mango-top.jpg",1)
 
-cv2.imshow("Mango de frente",img)
-cv2.imshow("Mango desde arriba",img2)
-cv2.waitKey(0)
+
+print(img2.shape)
--- preprocesamiento.py
+++ preprocesamiento.py
@@ -3,13 +3,19 @@ import cv2
 img = cv2.imread("img/mango-de-frente.png", 1)
 img2 = cv2.imread("img/mango-top.jpg",1)
 
+img = cv2.resize(img, (320, 320)) 
+
 imgGris = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 img2Gris = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+
+
+medianaImgGris = cv2.medianBlur(imgGris, 3)
+medianaImg2Gris = cv2.medianBlur(img2Gris, 3)
 
 cv2.imwrite('/../img/imgGris.jpg', imgGris)
 cv2.imwrite('/../img/img2Gris.jpg', img2Gris)
 
-cv2.imshow("Mango de frente en escala de grises", imgGris)
-cv2.imshow("Mango desde arriba en escala de grises", img2Gris)
+cv2.imshow("Mango de frente en escala de grises", medianaImgGris)
+cv2.imshow("Mango desde arriba en escala de grises", medianaImg2Gris)
 
 cv2.waitKey(0)
