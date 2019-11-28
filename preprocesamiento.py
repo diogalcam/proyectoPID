cf7e67bc875aa2285e33249c51a1d4ad7caa41ed
Dioni <dionigc96@gmail.com>
2019-11-28 15:56:45 +0100

    Editando

--- preprocesamiento.py
+++ preprocesamiento.py
@@ -1,6 +1,7 @@
 import cv2
 import os
 
+
 directory= r"C:\Users\dioni\git\proyectoPID\img"
 
 img = cv2.imread("img/mango-de-frente.png", 1)
@@ -11,15 +12,15 @@ img = cv2.resize(img, (320, 320))
 imgGris = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 img2Gris = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
 
-#medianaImgGris = cv2.medianBlur(imgGris, 3)
-#medianaImg2Gris = cv2.medianBlur(img2Gris, 3)
-
 blur = cv2.GaussianBlur(imgGris,(3,3),0)
 blur2 = cv2.GaussianBlur(img2Gris,(3,3),0)
 
-ret3,th3 = cv2.threshold(blur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-cv2.imshow("fff",th3)
+
+
+cv2.imshow("Segmentacion",blur)
 cv2.waitKey(0)
+
+
 
 os.chdir(directory)
 print("Before saving image:")   
@@ -39,4 +40,4 @@ print(os.listdir(directory))
 #cv2.imshow("Mango de frente en escala de grises con la mediana", medianaImgGris)
 #cv2.imshow("Mango desde arriba en escala de grises con la mediana", medianaImg2Gris)
 
-#cv2.waitKey(0)
+cv2.waitKey(0)
--- segmentacion.py
+++ segmentacion.py
@@ -1,8 +1,34 @@
 import cv2
 
-img = cv2.imread("img/imgGris.jpg", 1)
-img2 = cv2.imread("img/img2Gris.jpg", 1)
+img = cv2.imread("img/mango-de-frente.png", 1)
+img2 = cv2.imread("img/mango-top.jpg",1)
+
+img3 = cv2.imread("img/gaussImgGris.jpg", 1)
+img4 = cv2.imread("img/gaussImg2Gris.jpg", 1)
+
+def buscaMinimoBlanco(imagen):
+    filas,columnas,d = imagen.shape
+    for i in range(filas):
+        for j in range(columnas):
+            pixel = imagen[i,j]
+            minimo= min(pixel)
+    return minimo
 
 
-cv2.imshow("a", img)
+
+umbral = buscaMinimoBlanco(img3)
+for x in range(len(img3)):
+    for y in range(len(img3[0])):  
+        if(img3[x][y][0] < umbral-4):
+            img3[x][y] = 0
+
+umbral2 = buscaMinimoBlanco(img4)
+for x in range(len(img4)):
+    for y in range(len(img4[0])):
+        if(img4[x][y][0] < umbral2-4):
+            img4[x][y] = 0
+
+
+cv2.imshow("ff", img3)
+cv2.imshow("ff2", img4)
 cv2.waitKey(0)
