3860b8ef45b0228cc188becacbdcc73f7bc6c205
Iván <Ivn@192.168.1.136>
2020-01-11 17:18:29 +0100

    a

--- prueba.py
+++ prueba.py
@@ -84,8 +84,8 @@ def calculoVolumen(puntos):
 
 
 # Cargamos la imagen
-img = cv2.imread("img/mango-frente.jpg", 1)
-img2 = cv2.imread("img/mango-arriba.jpg",1)
+img = cv2.imread("img/naranja1-frente.jpg", 1)
+img2 = cv2.imread("img/naranja1-arriba.jpg",1)
 img3 = cv2.imread("img/naranja--frente.jpg",1)
 img4 = cv2.imread("img/naranja--arriba.jpg", 1)
 img = cv2.resize(img, (800, 600))
